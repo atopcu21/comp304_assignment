ce4f6a24b2e75f9cce0632714e62f947207fcd84
atopcu21 <atopcu21@ku.edu.tr>
2024-04-03 12:38:52 +0300

    pid's are not in order

--- part_a/code.c
+++ part_a/code.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    printf("%sHello, World!\n",argv[1]);
-    return 0;
+    if(argc <2){
+        return 1;
+    }
+    else{
+        int lvlCounter = 1;
+        int input = atoi(argv[1]);
+        printf("Main Process ID: %d, Parent ID: %d, level: %d\n",getpid(), getppid(), 0);
+        for(int i = 0; i < input; i++){
+            int id = fork();
+            if(id == 0){
+                //printf("child\n");
+                printf("Process ID: %d, Parent ID: %d, level: %d\n",getpid(), getppid(), lvlCounter++);
+                //printf("child\n");
+            }
+            //printf("Process ID: %d, Parent ID: %d, level: %d\n",getpid(), getppid(), lvlCounter++);
+        }
+    }
 }
